709e11f9c42570578fb2f6bc9c5f9b983e34e70e
Matheusdias77 <heyp692@gmail.com>
2024-07-26 10:26:31 -0300

    criando paginas

--- premium/src/main/java/controller/AdminController.java
+++ premium/src/main/java/controller/AdminController.java
@@ -1,9 +1,21 @@
 package controller;
 
+import domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.ItemCustomService;
+
+import java.util.List;
+
+/*Implemente a rota de (“/admin”) para, a partir de uma solicitação do tipo GET, gerar uma resposta
+contendo no corpo um HTML que contém uma tabela de todos os itens (linhas) que estão presentes
+no banco de dados e que não estão deletados (isDeleted == null). Para cada item listado adicione um
+link para a rota “/editar” e “/deletar” passando como parâmetro para tal rota o ID do item escolhido.
+Por fim, adicione na página gerada pela rota “/admin” um link para a rota “/cadastro”. (1,0 pontos)
+*/
 
 @Controller
 public class AdminController {
--- premium/src/main/java/controller/ItemController.java
+++ premium/src/main/java/controller/ItemController.java
@@ -1,2 +1,35 @@
-package controller;public class ItemController {
+package controller;
+
+import domain.Item;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.ItemCustomService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemCustomService itemService;
+
+    @GetMapping("/index")
+    public String item(Model model, HttpServletResponse response) {
+
+        Cookie visitCookie = new Cookie("visita", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        visitCookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(visitCookie);
+
+        return "index";
+    }
 }
